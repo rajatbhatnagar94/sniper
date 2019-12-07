4fd6bc432f855140194226b7adc78f98ed33a44b
Rajat Bhatnagar <rajatbhatnagar94@gmail.com>
2019-12-07 00:45:25 -0700

    added configurations of elk stack and their nginx configs

---
+++ elasticsearch/install.sh
@@ -0,0 +1,4 @@
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.0-amd64.deb
+sudo dpkg -i elasticsearch-7.5.0-amd64.deb
+sudo service elasticsearch start
+
---
+++ kibana/install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install default-jre
+sudo apt install default-jdk
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt update
+
