1b507d114f58d8d05208ed25cbf7725f71746a82
Rajat Bhatnagar <rajatbhatnagar94@gmail.com>
2019-12-09 13:40:36 -0700

    modified install.sh to send application logs as well

--- filebeat/install.sh
+++ filebeat/install.sh
@@ -1,8 +1,18 @@
+if [ -z "$1" ]
+then
+	echo "Please enter the name of the app log folder name"
+	exit 1
+fi
+app_log_dir_name=$1
 sudo apt update
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.5.0-amd64.deb
 sudo dpkg -i filebeat-7.5.0-amd64.deb
 sudo filebeat modules enable nginx
+sudo sed -i '24s! enabled: false! enabled: true!' /etc/filebeat/filebeat.yml
+sudo sed -i '28s!*!$app_log_dir_name/*!' /etc/filebeat/filebeat.yml
 sudo sed -i 's/#host:\s"localhost:5601"/host: "34.69.126.246:5601"/' /etc/filebeat/filebeat.yml
 sudo sed -i 's/localhost:9200/34.69.126.246:9201/' /etc/filebeat/filebeat.yml
 sudo filebeat setup --dashboards
 sudo service filebeat start
+
+
