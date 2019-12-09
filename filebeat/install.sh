c7757f3a58b8d8ebc71d9b9e9fe5c5832d436454
Rajat Bhatnagar <rajatbhatnagar94@gmail.com>
2019-12-09 15:07:06 -0700

    minor fixes in filebeat install script

--- filebeat/install.sh
+++ filebeat/install.sh
@@ -1,7 +1,7 @@
 if [ -z "$1" ]
 then
-	echo "Please enter the name of the app log folder name"
-	exit 1
+        echo "Please enter the name of the app log folder name"
+        exit 1
 fi
 app_log_dir_name=$1
 sudo apt update
@@ -11,8 +11,9 @@ sudo filebeat modules enable nginx
 sudo sed -i '24s! enabled: false! enabled: true!' /etc/filebeat/filebeat.yml
 sudo sed -i "28s!*!$app_log_dir_name/*!" /etc/filebeat/filebeat.yml
 sudo sed -i 's/#host:\s"localhost:5601"/host: "34.69.126.246:5601"/' /etc/filebeat/filebeat.yml
-sudo sed -i 's/localhost:9200/34.69.126.246:9201/' /etc/filebeat/filebeat.yml
+sudo sed -i 's/localhost:9200/34.69.126.246:9200/' /etc/filebeat/filebeat.yml
+sudo sed -i "31i\ \ fields:" /etc/filebeat/filebeat.yml
+sudo sed -i "32i\ \ \ \ appname:\ $app_log_dir_name" /etc/filebeat/filebeat.yml
 sudo filebeat setup --dashboards
 sudo service filebeat start
-
 
